d70641a07faa95e00f982f07a74db3323f40dd29
KKM <ccc1707@naver.com>
2026-07-29 10:59:00 +0900

    feat: implement configuration loader

--- app/config/config.py
+++ app/config/config.py
@@ -1,10 +1,7 @@
 from pathlib import Path
 import yaml
 
-print(__file__)
-
 config_path = Path(__file__).parent / "config.yaml"
-print(config_path)
 
 def load_config():
     with open(config_path) as file:
